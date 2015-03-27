9e816ebf8b17be6a54fdf0e56505bec60fa24a76
MarkJensen55 <markjensen@tds.net>
2015-03-27 16:43:04 -0400

    After some testing & start of collection

---
+++ StudentNinja/Main.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentNinja
+{
+    class Program
+    {
+        // exercise the student class for testing
+        // build a collection of students 
+        static void Main(string[] args)
+        {
+            
+            // test set up & print of a sample student
+            //var newStudent = new Student("Me");
+            //newStudent.CompleteProgram();
+            //newStudent.HelpAPeer();
+            //newStudent.HelpAPeer();
+            //newStudent.HelpAPeer();
+            //Console.WriteLine(newStudent.StudentName + " has rank of: " + newStudent.StudentRank);
+            //Console.ReadLine();
+
+            var classList = new Dictionary<string, Student>();
+            classList = MakeList();
+
+            var testName = "Keonna";
+            classList[testName].HelpAPeer();
+            classList[testName].CompleteProgram();
+            classList[testName].HelpAPeer();
+            Console.WriteLine(classList[testName].StudentName + " is a " + classList[testName].StudentRank);
+            Console.ReadLine();
+
+
+
+        }
+
+        // fake up the list of students for now
+        static Dictionary <string, Student> MakeList()
+        {
+            var studentList = new Dictionary <string, Student>();
+
+            studentList.Add("Nick", new Student("Nick"));
+            studentList.Add("Justin", new Student("Justin"));
+            studentList.Add("Keonna", new Student("Keonna"));
+            studentList.Add("Johnathon", new Student("Johnathon"));
+            studentList.Add("Mark", new Student("Mark"));
+            studentList.Add("Seth", new Student("Seth"));
+
+            return studentList;
+
+        }
+    }
+    
+}
---
+++ StudentNinja/Student.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentNinja
+{
+    // Student acheivement exercise for .net class
+    public class Student
+    {
+        private int studentLevel = 0;
+        private Rank studentRank = Rank.Beginner;
+
+        // class contructor requires name
+        public Student(string name) 
+        {
+            this.studentLevel = 0;
+            this.studentRank = Rank.Beginner;
+            this.StudentName = name;
+        }
+
+        // each time a student completes a program they uplevel by one point
+        // if level is 5 they inrcease rank & level is reset
+        // don't increase rank if already ninja
+        public void CompleteProgram()
+        {
+            studentLevel += 1;
+            if (studentLevel >= 5)
+            {
+                if (studentRank != Rank.Ninja)
+                {
+                    studentRank = studentRank + 1;
+                }
+                studentLevel = studentLevel -5;
+            }
+
+        }
+
+        // each time a student helps a peer they uplevel by two points
+        // if level is 5 they inrcease rank & level is reset
+        // don't increase rank if already ninja
+        public void HelpAPeer()
+        {
+            studentLevel += 2;
+            if (studentLevel >= 5)
+            {
+                if (studentRank != Rank.Ninja)
+                {
+                    studentRank = studentRank + 1;
+                    studentLevel = studentLevel - 5;
+                }
+            }
+
+        }
+
+        public string StudentName
+        {
+            get;
+            private set;
+        }
+
+        public Rank StudentRank
+        {
+            get { return studentRank; }
+         
+        }
+    }
+
+
+
+    public enum Rank
+    {
+        Beginner,
+        Grasshopper,
+        Journeyman,
+        RockStar,
+        Ninja,
+        Jedi
+    }
+}
