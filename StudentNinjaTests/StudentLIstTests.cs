3e05fc04ddf9b1f5f21c3602c188b496bddc237d
MarkJensen55 <markjensen@tds.net>
2015-03-29 12:58:48 -0400

    tests for adds & inlist
    teset

--- StudentNinjaTests/StudentLIstTests.cs
+++ StudentNinjaTests/StudentLIstTests.cs
@@ -21,5 +21,34 @@ namespace StudentNinjaTests
 
         }
 
+        [Fact]
+        public void ListNamePropertyIsReturned()
+        {
+            var testList = new StudentList("Another Class");
+
+            Assert.Equal("Another Class", testList.ListName);
+        }
+
+        [Fact]
+        public void InListMethodWithStudentName()
+        {
+            var testList = new StudentList("Econ 101");
+
+            testList.Add(new Student("Nick"));
+            testList.Add(new Student("Justin"));
+            testList.Add(new Student("Keonna"));
+            testList.Add(new Student("Johnathon"));
+            var secondStudent = new Student("That Guy");
+            testList.Add(secondStudent);
+            testList.Add(new Student("Mark"));
+            testList.Add(new Student("Seth"));
+
+            Assert.True(testList.InList("Keonna"));
+            Assert.False(testList.InList("Keona"));
+            Assert.True(testList.InList(secondStudent));
+
+
+        }
+
     }
 }
