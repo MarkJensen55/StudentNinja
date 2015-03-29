b8673bfe387306c223c4a42b7d2e5150afd2c90e
MarkJensen55 <markjensen@tds.net>
2015-03-29 10:03:45 -0400

    passed empty list tests

--- StudentNinjaTests/StudentLIstTests.cs
+++ StudentNinjaTests/StudentLIstTests.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using StudentNinja;
+using Xunit;
 
 namespace StudentNinjaTests
 {
     public class StudentLIstTests
     {
+        [Fact]
+        public void InListShouldReturnFalseOnEmptyList()
+        {
+            var testStudent = new Student("My name is Test");
+            var testLIst = new StudentList("Econ 101");
+
+            Assert.False(testLIst.InList("My name is Test"));
+            Assert.False(testLIst.InList(testStudent));
+
+        }
+
     }
 }
